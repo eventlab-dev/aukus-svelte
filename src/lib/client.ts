62bb1263103a1762450b8da1631690cd3ee123d7
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-09-29 11:45:35 +0200

    Use environment variables for base URLs
    The base URLs for Eventlab and Aukus are now configured using environment variables. This
    allows for easier configuration of different environments.

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,10 +2,5 @@ import { QueryClient } from '@tanstack/svelte-query'
 
 export const queryClient = new QueryClient()
 
-export const EventlabBaseUrl = import.meta.env.DEV
-	? 'http://localhost:8300'
-	: 'https://api.eventlab.dev'
-
-export const AukusBaseUrl = import.meta.env.DEV
-	? 'http://localhost:8301'
-	: 'https://aukus.eventlab.dev'
+export const EventlabBaseUrl = import.meta.env.VITE_EVENTLAB_BASE_URL
+export const AukusBaseUrl = import.meta.env.VITE_AUKUS_BASE_URL
