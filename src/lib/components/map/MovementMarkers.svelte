06728924a926551eb6dc94c6973c13f7f1ee8fbc
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-10-03 00:13:29 +0200

    Show movement markers and dice roll chance
    - Show movement markers on the map to preview the move - Show the chance of landing on a ladder
    or snake

--- src/lib/components/map/MovementMarkers.svelte
+++ src/lib/components/map/MovementMarkers.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { getAppManagerContext } from '$lib/contexts/appManagerContext'
-	import { getCellPosition } from '$lib/mapUtils'
+	import { getCellPosition, laddersByCell, snakesByCell } from '$lib/mapUtils'
 
 	const { movementStore } = getAppManagerContext()
 	const { movementParams } = movementStore
-
-	const direction = $derived($movementParams?.direction ?? 'forward')
 
 	const cells = $derived.by(() => {
 		if (!$movementParams) return null
@@ -14,17 +12,26 @@
 		for (let i = 1; i <= Math.abs(steps); i++) {
 			cells.push(direction === 'forward' ? startCell + i : startCell - i)
 		}
-		return cells.map((cell) => getCellPosition(cell))
+		return cells.map((cell) => ({ id: cell, position: getCellPosition(cell) }))
 	})
+
+	function cellType(cellId: number) {
+		if (snakesByCell[cellId]) {
+			return 'negative'
+		}
+		if (laddersByCell[cellId]) {
+			return 'positive'
+		}
+		return 'neutral'
+	}
 </script>
 
 {#if $movementParams && cells}
 	{#each cells as cell, idx (idx)}
 		<div
-			class="absolute w-10 rounded-2xl bg-black p-3 text-center"
-			style="top: {cell.y + 40}px; left: {cell.x + 45}px; background-color: {direction === 'forward'
-				? '#34C759'
-				: '#FF3B30'}"
+			class="absolute w-10 rounded-2xl p-3 text-center data-[variant=negative]:bg-red-500 data-[variant=neutral]:bg-secondary data-[variant=positive]:bg-green-500"
+			data-variant={cellType(cell.id)}
+			style="top: {cell.position.y + 40}px; left: {cell.position.x + 45}px;"
 		>
 			<div>{idx + 1}</div>
 		</div>
--- src/lib/components/moveForm/DiceRoller.svelte
+++ src/lib/components/moveForm/DiceRoller.svelte
@@ -4,20 +4,17 @@
 	import { ToggleGroupItem } from '../ui/toggle-group'
 	import ToggleGroup from '../ui/toggle-group/toggle-group.svelte'
 	import type { DiceOption } from '$lib/heyapi/aukus/types.gen'
+	import { laddersByCell, snakesByCell } from '$lib/mapUtils'
 
 	const { usersStore, eventDataStore, movementStore, myPlayer } = getAppManagerContext()
 	const { finishMove, rollDice } = usersStore
 	const { diceOptions, eventDataQuery, myLastMove } = eventDataStore
 
-	const defaultOption = $derived($diceOptions[$diceOptions.length - 1] ?? null)
+	let currentOption = $state<DiceOption>($diceOptions[$diceOptions.length - 1])
 
-	let currentOption = $state<DiceOption | null>(null)
-
-	$effect(() => {
-		if (defaultOption) {
-			currentOption = defaultOption
-		}
-	})
+	const moveDirection = $derived(
+		$myLastMove?.type === 'drop' || $myLastMove?.type === 'sheikh_moment' ? -1 : 1
+	)
 
 	async function handleThrowDice() {
 		const rollResult = await $rollDice.mutateAsync({
@@ -32,25 +29,82 @@
 			}
 		})
 
-		let rollSum = rollResult.roll_values.reduce((a, b) => a + b, 0)
-		if ($myLastMove!.type === 'drop' || $myLastMove!.type === 'sheikh_moment') {
-			rollSum = -rollSum
-		}
+		const rollSteps = rollResult.roll_values.reduce((a, b) => a + b, 0) * moveDirection
 
 		movementStore.movePlayer({
 			playerSlug: $myPlayer!.slug,
-			steps: rollSum
+			steps: rollSteps
 		})
 		$eventDataQuery.refetch()
 	}
+
+	const { ladderChance, snakeChance, maxRoll } = $derived.by(() => {
+		if (currentOption === null || !$myPlayer) {
+			return { ladderChance: 0, snakeChance: 0, maxRoll: 0 }
+		}
+		let maxRoll = 0
+		switch (currentOption) {
+			case '1d6':
+				maxRoll = 6
+				break
+			case '2d6':
+				maxRoll = 12
+				break
+			case '3d6':
+				maxRoll = 18
+				break
+			case '1d4':
+				maxRoll = 4
+				break
+			default: {
+				const error: never = currentOption
+				throw new Error(`Unsupported dice option: ${error}`)
+			}
+		}
+		let ladders = 0
+		let snakes = 0
+		for (let i = 1; i <= maxRoll; i++) {
+			const cellId = $myPlayer.map_position + i * moveDirection
+			if (laddersByCell[cellId]) {
+				ladders++
+			}
+			if (snakesByCell[cellId]) {
+				snakes++
+			}
+		}
+		return {
+			ladderChance: (ladders / maxRoll) * 100,
+			snakeChance: (snakes / maxRoll) * 100,
+			maxRoll
+		}
+	})
+
+	$effect(() => {
+		if (currentOption && $myPlayer) {
+			movementStore.movementParams.set({
+				direction: moveDirection === 1 ? 'forward' : 'backward',
+				startCell: $myPlayer.map_position,
+				steps: maxRoll * moveDirection
+			})
+		}
+	})
+
+	function getDiceOption() {
+		return currentOption
+	}
+
+	function setDiceOption(value: string) {
+		if (value) {
+			currentOption = value as DiceOption
+		}
+	}
 </script>
 
-{#if $diceOptions.length > 1 && defaultOption}
+{#if $diceOptions.length > 1 && currentOption}
 	<div class="w-100 rounded-2xl bg-card p-5">
 		<ToggleGroup
 			type="single"
-			value={defaultOption}
-			onValueChange={(value) => (currentOption = value as DiceOption)}
+			bind:value={getDiceOption, setDiceOption}
 			variant="outline"
 			class="w-full"
 		>
@@ -61,21 +115,21 @@
 			{/each}
 		</ToggleGroup>
 		<div class="py-5">
-			<p>Шанс лестницы: 5%</p>
-			<p>Шанс змейки: 5%</p>
+			<p>Шанс лестницы: {ladderChance.toFixed(1)}%</p>
+			<p>Шанс змейки: {snakeChance.toFixed(1)}%</p>
 		</div>
-		<Button class="mt-5 w-full" onclick={handleThrowDice}>Бросить кубики</Button>
+		<Button class="mt-5 w-full" onclick={handleThrowDice}>Бросить кубики {currentOption}</Button>
 	</div>
-{:else if $diceOptions.length === 1 && defaultOption}
+{:else if $diceOptions.length === 1 && currentOption}
 	<div class="w-100 rounded-2xl bg-card p-5">
-		<p class="mb-5">Шанс лестницы: 5%</p>
-		<p class="mb-5">Шанс змейки: 5%</p>
+		<p class="mb-5">Шанс лестницы: {ladderChance.toFixed(1)}%</p>
+		<p class="mb-5">Шанс змейки: {snakeChance.toFixed(1)}%</p>
 		<Button
 			class="mt-5 w-full"
 			onclick={handleThrowDice}
 			loading={$rollDice.isPending || $finishMove.isPending}
 		>
-			Бросить кубики {defaultOption}
+			Бросить кубики {currentOption}
 		</Button>
 	</div>
 {/if}
