f9ecd422ff73a2c22450585e54523ea1c4b3bcc8
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-10-05 16:08:40 +0200

    Fix: Only show dice options when player is defined
    The dice panel was showing options even when the player was not defined, leading to errors.
    
    The movement markers now show a negative sign when the movement is negative.

--- src/lib/components/map/MovementMarkers.svelte
+++ src/lib/components/map/MovementMarkers.svelte
@@ -3,7 +3,7 @@
 	import { getCellPosition, laddersByCell, snakesByCell } from '$lib/mapUtils'
 
 	const { movementStore, turnState } = getAppManagerContext()
-	const { movementState } = movementStore
+	const { movementState, selectedPlayer } = movementStore
 
 	const cells = $derived.by(() => {
 		const { startCell, steps } = $movementState
@@ -13,6 +13,8 @@
 		}
 		return cells.map((cell) => ({ id: cell, position: getCellPosition(cell) }))
 	})
+
+	const showMinus = $derived($movementState.steps < 0)
 
 	function cellType(cellId: number) {
 		if (snakesByCell[cellId]) {
@@ -27,18 +29,19 @@
 	const showMarkers = $derived(
 		$turnState === 'selecting-dice' ||
 			$turnState === 'dice-results' ||
-			$turnState === 'player-animation'
+			$turnState === 'player-animation' ||
+			$selectedPlayer
 	)
 </script>
 
 {#if cells && showMarkers}
 	{#each cells as cell, idx (idx)}
 		<div
-			class="absolute w-10 rounded-2xl p-3 text-center data-[variant=negative]:bg-red-500 data-[variant=neutral]:bg-secondary data-[variant=positive]:bg-green-500"
+			class="absolute w-fit rounded-2xl p-3 text-center data-[variant=negative]:bg-red-500 data-[variant=neutral]:bg-secondary data-[variant=positive]:bg-green-500"
 			data-variant={cellType(cell.id)}
 			style="top: {cell.position.y + 40}px; left: {cell.position.x + 45}px;"
 		>
-			<div>{idx + 1}</div>
+			<div>{showMinus ? '-' : '+'}{idx + 1}</div>
 		</div>
 	{/each}
 {/if}
