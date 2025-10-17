441c1c857a074a2c3fd583c9c75bdd83040995fb
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-10-17 23:05:10 +0200

    Display only one achievement notification at a time
    The component was previously looping through all achievements in the `achievements` store,
    displaying a dialog for each. This change modifies the component to only display the first
    achievement in the store.

--- src/lib/components/achievements/AchievementsNotifications.svelte
+++ src/lib/components/achievements/AchievementsNotifications.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { AchievementBackgroundUrl } from '$lib/constants'
 	import { getAppManagerContext } from '$lib/contexts/appManagerContext'
+	import type { AchievementItem } from '$lib/heyapi/aukus/types.gen'
 	import { getSkinIconUrl } from '$lib/utils'
 	import { Button } from '../ui/button'
 	import { Dialog, DialogClose, DialogContent } from '../ui/dialog'
@@ -8,24 +9,34 @@
 	const { notificationStore, eventDataStore } = getAppManagerContext()
 	const { achievements, hideNotification } = notificationStore
 	const { skinsById } = eventDataStore
+
+	const achievement = $derived<AchievementItem | undefined>($achievements[0])
+
+	const handleOpenChange = (isOpen: boolean) => {
+		if (!isOpen && achievement) {
+			hideNotification(achievement.id)
+		}
+	}
 </script>
 
-{#each $achievements as achievement (achievement.id)}
-	<Dialog open onOpenChange={() => hideNotification(achievement.id)}>
-		<DialogContent showCloseButton={false} class="w-fit">
-			<div class="mb-0 flex justify-center text-lg">🏆 Достижение разблокировано!</div>
-			<div class="relative flex justify-center">
-				<img class="h-40 rounded-2xl" src={AchievementBackgroundUrl} alt="background" />
-				<img
-					class="absolute top-1/2 left-1/2 h-16 -translate-x-1/2 -translate-y-1/2"
-					src={getSkinIconUrl($skinsById.get(achievement.reward_skin_id)?.image_url ?? '')}
-					alt="skin"
-				/>
-			</div>
-			<div>{achievement.description}</div>
-			<DialogClose onclick={() => hideNotification(achievement.id)}>
-				<Button>Круто!</Button>
-			</DialogClose>
-		</DialogContent>
-	</Dialog>
-{/each}
+{#if achievement}
+	{#key achievement.id}
+		<Dialog open onOpenChange={handleOpenChange}>
+			<DialogContent showCloseButton={false} class="w-fit">
+				<div class="mb-0 flex justify-center text-lg">🏆 Достижение разблокировано!</div>
+				<div class="relative flex justify-center">
+					<img class="h-40 rounded-2xl" src={AchievementBackgroundUrl} alt="background" />
+					<img
+						class="absolute top-1/2 left-1/2 h-16 -translate-x-1/2 -translate-y-1/2"
+						src={getSkinIconUrl($skinsById.get(achievement.reward_skin_id)?.image_url ?? '')}
+						alt="skin"
+					/>
+				</div>
+				<div>{achievement.description}</div>
+				<DialogClose>
+					<Button>Круто!</Button>
+				</DialogClose>
+			</DialogContent>
+		</Dialog>
+	{/key}
+{/if}
