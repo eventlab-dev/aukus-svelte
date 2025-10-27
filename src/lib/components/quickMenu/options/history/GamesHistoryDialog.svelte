b8878576518dc1102546a9e2596bd6bf6318cc00
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-10-27 10:53:15 +0100

    feat: Add player filter to game history dialog

--- src/lib/components/quickMenu/options/history/GamesHistoryDialog.svelte
+++ src/lib/components/quickMenu/options/history/GamesHistoryDialog.svelte
@@ -12,8 +12,10 @@
 	import LoaderCircle from '@lucide/svelte/icons/loader-circle'
 	import GameCard from './GameCard.svelte'
 	import { ScrollArea } from '$lib/components/ui/scroll-area'
+	import { Toggle } from '$lib/components/ui/toggle'
+	import X from '@lucide/svelte/icons/x'
 
-	const { gamesHistoryStore } = getAppManagerContext()
+	const { gamesHistoryStore, players, playersBySlug } = getAppManagerContext()
 	const { gamesHistory, searchParams, historyQuery } = gamesHistoryStore
 
 	let timer: number = 0
@@ -26,7 +28,15 @@
 		}
 	}
 
-	$inspect($gamesHistory)
+	let selectedPlayer = $state<string | null>(null)
+
+	function selectPlayer(_: boolean, slug: string) {
+		if (selectedPlayer === slug) {
+			selectedPlayer = null
+		} else {
+			selectedPlayer = slug
+		}
+	}
 </script>
 
 <Dialog>
@@ -39,7 +49,23 @@
 		<DialogHeader class="gap-3">
 			<DialogTitle class="text-2xl font-bold">История игр</DialogTitle>
 		</DialogHeader>
-		<div class="mt-10">
+		<div class="mt-10 flex flex-col gap-5">
+			<div class="flex w-full flex-wrap gap-2">
+				{#each $players as player (player.slug)}
+					<Toggle
+						variant="outline"
+						bind:pressed={() => player.slug === selectedPlayer, (v) => selectPlayer(v, player.slug)}
+						class="cursor-pointer"
+					>
+						{player.username}
+						{#if selectedPlayer === player.slug}
+							<span class="rounded bg-white/20 p-0.5">
+								<X class="stroke-4" />
+							</span>
+						{/if}
+					</Toggle>
+				{/each}
+			</div>
 			<ScrollArea class="h-[70vh] w-full" type="always">
 				<Input
 					type="text"
@@ -59,7 +85,9 @@
 					{:else}
 						<div class="flex flex-col gap-10">
 							{#each $gamesHistory as game (game.id)}
-								<GameCard {game} />
+								{#if $playersBySlug[game.player_name] !== undefined}
+									<GameCard {game} />
+								{/if}
 							{/each}
 						</div>
 					{/if}
