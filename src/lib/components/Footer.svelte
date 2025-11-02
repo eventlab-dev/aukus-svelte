9db779b50018621567311d595e3899fc488c8c45
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-02 23:51:49 +0100

    Refactor Footer component
    - Changed content of the footer - Changed layout of the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,41 +3,15 @@
 
 	const sections = [
 		{
-			title: 'AUKUS-DEVS',
+			title: 'EventLab',
 			items: [
 				{
 					label: 'Поддержать разработчиков',
-					href: '#'
+					href: 'https://boosty.to/aukus'
 				},
 				{
 					label: 'Мы на Гитхабе',
-					href: '#'
-				}
-			]
-		},
-		{
-			title: 'Уведомления',
-			items: [
-				{
-					label: 'Канал в телеграме',
-					href: '#'
-				},
-				{
-					label: 'Канал в дискорде',
-					href: '#'
-				}
-			]
-		},
-		{
-			title: 'Зеркала сайта',
-			items: [
-				{
-					label: 'aukus.fun',
-					href: '#'
-				},
-				{
-					label: 'aukus.su',
-					href: '#'
+					href: 'https://github.com/eventlab-dev'
 				}
 			]
 		},
@@ -45,29 +19,31 @@
 			title: 'Связь с нами',
 			items: [
 				{
-					label: 'Наш дискорд',
-					href: '#'
+					label: 'Телеграм',
+					href: 'https://t.me/eventlabdev'
 				},
 				{
-					label: 'Помогите стать лучше',
-					href: '#'
+					label: 'Почта для отзывов',
+					href: 'mailto:contact@eventlab.dev'
 				}
 			]
 		}
 	]
 </script>
 
-<footer class="mt-[100px] flex justify-between rounded-xl bg-card px-[120px] py-[50px]">
-	{#each sections as { title, items } (title)}
-		<div class="space-y-[25px]">
-			<div class="text-xl leading-[24px] font-bold">{title}</div>
-			<div class="mt-4 flex flex-col gap-[15px]">
-				{#each items as { label, href } (label)}
-					<Button {href} variant="link" class="h-auto p-0 text-sm font-medium text-foreground ">
-						{label}
-					</Button>
-				{/each}
+<footer class="mt-[100px] flex justify-center rounded-xl bg-card px-[120px] py-[50px]">
+	<div class="flex w-[700px] justify-between">
+		{#each sections as { title, items } (title)}
+			<div class="space-y-[25px]">
+				<div class="text-xl leading-[24px] font-bold">{title}</div>
+				<div class="mt-4 flex flex-col gap-[15px]">
+					{#each items as { label, href } (label)}
+						<Button {href} variant="link" class="h-auto p-0 text-sm font-medium text-foreground ">
+							{label}
+						</Button>
+					{/each}
+				</div>
 			</div>
-		</div>
-	{/each}
+		{/each}
+	</div>
 </footer>
