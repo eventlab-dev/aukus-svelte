54e0f47240257668b7f8ef6f233cd73450c3633a
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-11 00:50:46 +0100

    Add twallpaper background to CommonWrapper component

--- src/lib/components/CommonWrapper.svelte
+++ src/lib/components/CommonWrapper.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { TWallpaper } from 'twallpaper'
+	import 'twallpaper/css'
 	import { flip } from 'svelte/animate'
 	import Footer from './Footer.svelte'
 	import Navigation from './Navigation.svelte'
@@ -7,6 +9,7 @@
 	import { ScrollArea } from './ui/scroll-area'
 	import { createAppManager } from '$lib/stores/AppManager.svelte'
 	import { setAppManagerContext } from '$lib/contexts/appManagerContext'
+	import { onMount } from 'svelte'
 
 	let { children } = $props()
 
@@ -16,9 +19,32 @@
 	const { players } = appManager
 
 	const sortedPlayers = $derived([...$players].sort((a, b) => b.map_position - a.map_position))
+
+	onMount(() => {
+		const container = document.getElementById('wallpaper') as HTMLElement
+		if (!container) return
+
+		const wallpaper = new TWallpaper(container, {
+			fps: 1,
+			tails: 5,
+			animate: false,
+			scrollAnimate: false,
+			colors: ['#1c1c1c', '#1c1c1c', '#1c1c1c', '#1c1c1c'],
+			pattern: {
+				image: 'https://twallpaper.js.org/patterns/games.svg',
+				background: '#0c0c0c',
+				blur: 0,
+				size: '470px',
+				opacity: 1,
+				mask: true
+			}
+		})
+		wallpaper.init()
+	})
 </script>
 
 <ScrollArea class="h-screen px-3" type="always" id="main-scroll-area">
+	<div id="wallpaper"></div>
 	<div class="py-3">
 		<div class="absolute top-3 left-3 z-10">
 			<QuickMenu />
