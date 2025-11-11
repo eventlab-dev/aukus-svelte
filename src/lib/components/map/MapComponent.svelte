a12304b6a80fa5c17d3c1df8b89856d6690a0b63
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-11 12:17:41 +0100

    Move logo images outside scrollable map container

--- src/lib/components/map/MapComponent.svelte
+++ src/lib/components/map/MapComponent.svelte
@@ -26,12 +26,13 @@
 	}
 </script>
 
+<div class="relative flex w-full justify-center">
+	<img src={LOGO_URL} class="absolute top-[100px] w-[300px]" alt="logo" />
+	<img src={LOGO_BG_URL} alt="back" />
+</div>
+
 <div class="flex justify-center">
 	<div class="overflow-x-auto overflow-y-hidden" id="map-scroll-container">
-		<div class="relative flex w-full justify-center">
-			<img src={LOGO_URL} class="absolute top-[100px] w-[300px]" alt="logo" />
-			<img src={LOGO_BG_URL} alt="back" />
-		</div>
 		<button id={MapContainerId} class="relative h-[2000px] w-[1700px]" onclick={handleClick}>
 			<img class="h-[2000] w-[1700px] bg-center bg-no-repeat" src={MAP_IMAGE} alt="map" />
 			<MapArrowMarkers />
