9f5cc3843c4a8110394f3c433eb4b79ee5823944
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-11 23:53:45 +0100

    Increase collapsed opacity in CollapsibleContent to 0.75

--- src/lib/components/collapsible/CollapsibleContent.svelte
+++ src/lib/components/collapsible/CollapsibleContent.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
-	import { getCollapsibleContext } from './collapsibleContext';
+	import type { Snippet } from 'svelte'
+	import { getCollapsibleContext } from './collapsibleContext'
 
 	type Props = {
-		class?: string;
-		children: Snippet;
-	};
+		class?: string
+		children: Snippet
+	}
 
-	const { class: className, children }: Props = $props();
+	const { class: className, children }: Props = $props()
 
-	const collapsible = getCollapsibleContext();
-	const minCollapsedHeight = 6;
+	const collapsible = getCollapsibleContext()
+	const minCollapsedHeight = 6
 
-	let height = $state(0);
-	let contentRef: HTMLDivElement | null = $state(null);
+	let height = $state(0)
+	let contentRef: HTMLDivElement | null = $state(null)
 
 	$effect(() => {
 		if (contentRef) {
 			// hack to prevent "justify-content: end" affecting height calculation
-			contentRef.style.display = 'block';
-			height = !collapsible.isCollapsed ? contentRef.scrollHeight : minCollapsedHeight;
-			contentRef.style.display = 'flex';
+			contentRef.style.display = 'block'
+			height = !collapsible.isCollapsed ? contentRef.scrollHeight : minCollapsedHeight
+			contentRef.style.display = 'flex'
 		}
-	});
+	})
 </script>
 
 <div
 	bind:this={contentRef}
 	style="height: {height}px;"
 	class={[
-		'z-[-1] mx-auto flex w-full flex-col justify-end space-y-[3px] overflow-hidden transition-[opacity,scale,height] duration-300 group-data-[collapsed=true]:pointer-events-none group-data-[collapsed=true]:scale-x-95 group-data-[collapsed=true]:opacity-50',
+		'z-[-1] mx-auto flex w-full flex-col justify-end space-y-[3px] overflow-hidden transition-[opacity,scale,height] duration-300 group-data-[collapsed=true]:pointer-events-none group-data-[collapsed=true]:scale-x-95 group-data-[collapsed=true]:opacity-75',
 		className
 	]}
 >
