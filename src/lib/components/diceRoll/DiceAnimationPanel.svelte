c80229eeea0a5bf6a1d70dc13afd772f8515f06c
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-25 12:10:57 +0100

    Increase DiceAnimationPanel width from 600px to 700px

--- src/lib/components/diceRoll/DiceAnimationPanel.svelte
+++ src/lib/components/diceRoll/DiceAnimationPanel.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
-	<div class="fixed top-1/2 left-1/2 h-[300px] w-[600px] -translate-x-1/2 -translate-y-1/2 rounded">
+	<div class="fixed top-1/2 left-1/2 h-[300px] w-[700px] -translate-x-1/2 -translate-y-1/2 rounded">
 		<Canvas toneMapping={NoToneMapping} colorSpace={LinearSRGBColorSpace}>
 			<T.Group scale={1.5}>
 				<DiceAnimationScene />
