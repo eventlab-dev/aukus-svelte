7ea9943cc8a2402273ce8ec5b44031b187b4c645
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-12-02 23:54:18 +0100

    Refactor player stats into PlayerStats component

--- src/routes/presentation/+page.svelte
+++ src/routes/presentation/+page.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '$lib/components/ui/button'
 	import { getAppManagerContext } from '$lib/contexts/appManagerContext'
 	import TotalStats from './components/TotalStats.svelte'
-	import StatsCards from './components/StatsCards.svelte'
+	import PlayerStats from './components/PlayerStats.svelte'
 
 	const { playersInOrder } = getAppManagerContext()
 
@@ -22,13 +22,6 @@
 	const onLastPage = $derived(pageId + 1 === pagesAmount)
 
 	const currentPlayer = $derived($playersInOrder[$playersInOrder.length - pageId] ?? null)
-
-	const playerStats = $derived.by(() => {
-		if (currentPlayer) {
-			return [{ title: 'Всего', value: '100' }]
-		}
-		return []
-	})
 </script>
 
 <div class="flex w-full justify-between px-4">
@@ -52,18 +45,7 @@
 		<TotalStats />
 	</div>
 {:else if currentPlayer}
-	<div class="mt-[100px] flex flex-col justify-center">
-		<div class="mb-[20px] text-center">
-			<div class="text-xl font-bold italic">{$playersInOrder.length - pageId + 1}-ое место</div>
-			<div class="text-6xl font-bold">
-				{currentPlayer.first_name}
-				{currentPlayer.username}
-			</div>
-		</div>
-		<div class="flex max-w-[1100px] justify-center">
-			<StatsCards stats={playerStats} />
-		</div>
-	</div>
+	<PlayerStats player={currentPlayer} position={$playersInOrder.length - pageId + 1} />
 {:else if pageId === pagesAmount - 1}
 	<div class="mt-[200px] flex justify-center">
 		<div class="text-3xl">Титры</div>
---
+++ src/routes/presentation/components/PlayerStats.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { PlayerData } from '$lib/types'
+	import StatsCards from './StatsCards.svelte'
+
+	type Props = {
+		player: PlayerData
+		position: number
+	}
+
+	let { player, position }: Props = $props()
+
+	const playerStats = $derived.by(() => {
+		return [{ title: 'Всего', value: '100' }]
+	})
+</script>
+
+<div class="mt-[100px] flex flex-col">
+	<div class="mb-[20px] w-full text-center">
+		<div
+			class="text-xl font-bold text-[var(--player-color)] italic"
+			style={`--player-color: ${player.color}`}
+		>
+			{position}-ое место
+		</div>
+		<div class="text-6xl font-bold">
+			{player.first_name}
+			{player.username}
+		</div>
+	</div>
+	<div class="flex w-full justify-center">
+		<div class="flex max-w-[1100px] justify-center">
+			<StatsCards stats={playerStats} />
+		</div>
+	</div>
+</div>
