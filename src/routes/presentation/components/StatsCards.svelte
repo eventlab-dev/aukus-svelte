6e4e02a39eeb63fcd2f2056fce2febb3091ce79a
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-12-02 02:11:34 +0100

    Add StatsCards and TotalStats components for presentation stats

---
+++ src/routes/presentation/components/StatsCards.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	type Stat = {
+		title: string
+		value: string
+	}
+
+	type Props = {
+		stats: Stat[]
+	}
+
+	let { stats }: Props = $props()
+</script>
+
+<div class="flex flex-wrap gap-3">
+	{#each stats as s (s.title)}
+		<div class="w-fit rounded-xl bg-card p-4">
+			<div class="mb-3 text-[#9F9F9F]">{s.title}</div>
+			<div class="text-bold text-4xl">{s.value}</div>
+		</div>
+	{/each}
+</div>
---
+++ src/routes/presentation/components/TotalStats.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import StatsCards from './StatsCards.svelte'
+
+	const stats = [
+		{ title: 'Total games', value: '100' },
+		{ title: 'Full', value: '10/100' }
+	]
+</script>
+
+<div class="max-w-[1100px]">
+	<div class="mb-7 text-center text-6xl font-bold">Общая статистика</div>
+	<div class="flex justify-center">
+		<StatsCards {stats} />
+	</div>
+</div>
