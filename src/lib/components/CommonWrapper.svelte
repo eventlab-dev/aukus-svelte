dd5cc9014b59a94ea11555b94f0b290fc7cbef62
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-07-29 21:19:17 +0200

    remove unused code

--- src/lib/components/CommonWrapper.svelte
+++ src/lib/components/CommonWrapper.svelte
@@ -3,8 +3,6 @@
 	import Navigation from './Navigation.svelte'
 	import QuickMenu from './quickMenu/QuickMenu.svelte'
 	import { AppManager, setAppManager } from '$lib/stores/AppManager.svelte'
-	import { onMount } from 'svelte'
-	import PlayersList from './player/PlayersList.svelte'
 	import MobilePage from './mobile/MobilePage.svelte'
 	import ErrorNotifications from './ErrorNotifications.svelte'
 	import { setErrorCallback } from '$lib/client'
@@ -37,24 +35,7 @@
 	<MobilePage />
 {:else}
 	<div class="">
-		{#if !hidePanels}
-			<div class="absolute top-3 left-3 z-10">
-				<QuickMenu />
-			</div>
-
-			<div class="absolute top-3 right-3 z-10 flex hidden flex-col gap-1.5">
-				<PlayersList />
-			</div>
-
-			<Navigation />
-		{/if}
 		{@render children?.()}
-		{#if !hidePanels}
-			<div class="px-3">
-				<Footer />
-			</div>
-			
-		{/if}
 		<SmartphoneNav />
 	</div>
 {/if}
