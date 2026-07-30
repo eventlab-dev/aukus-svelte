8d8f31ac9887104f9fe69781aa879f7240722fbd
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-07-30 02:00:38 +0200

    add tier tooltip

---
+++ src/lib/components/tierlist/GamePopup.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import type { PlayerMoveItem } from '$lib/heyapi/aukus/types.gen'
+	import { formatMs, renderToHTML } from '$lib/utils'
+
+	type Props = {
+		game: PlayerMoveItem
+	}
+
+	let { game }: Props = $props()
+
+    const parsedReview = $derived(renderToHTML(game.item_review))
+
+	const statusText = $derived.by(() => {
+        const time = formatMs(game.item_duration)
+        let diffText = ''
+        switch (game.difficulty_level) {
+            case -1:
+                diffText = ' на легком'
+                break
+            case 0:
+                diffText = ''
+                break
+            case 1:
+                diffText = ' на сложном'
+                break
+            case 2:
+                diffText = ' на очень сложном'
+                break
+            default: {
+                const error: never = game.difficulty_level
+                throw new Error(`Unknown difficulty level: ${error}`)
+            }
+        }
+
+		switch (game.type) {
+			case 'completed': 
+				return `Пройдено за ${time}${diffText}`
+			case 'drop':
+				return `Дроп через ${time}${diffText}`
+			case 'movie':
+				return `Просмотр ${time}`
+			case 'reroll':
+				return `Реролл`
+			case 'sheikh_moment':
+				return `Шейх-дроп через ${time}${diffText}`
+			default: {
+				const error: never = game.type
+				throw new Error(`Unknown game type: ${error}`)
+			}
+		}
+	})
+</script>
+
+<div class="max-w-[500px] p-2">
+	<div class="flex flex-col gap-2">
+		<div class="text-xl">{game.item_title}</div>
+		<div>{statusText}</div>
+        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		<div class="[&>*]:inline">{game.item_rating}/10 — {@html parsedReview}</div>
+	</div>
+</div>  
--- src/lib/components/tierlist/TierList.svelte
+++ src/lib/components/tierlist/TierList.svelte
@@ -1,62 +1,80 @@
 <script lang="ts">
-	import { TIERS, type Tier } from "$lib/constants"
-	import type { PlayerMoveItem } from "$lib/heyapi/aukus/types.gen"
-	import { scoreToTier } from "$lib/utils"
+	import { TIERS, type Tier } from '$lib/constants'
+	import type { PlayerMoveItem } from '$lib/heyapi/aukus/types.gen'
+	import { scoreToTier } from '$lib/utils'
+	import { Popover, PopoverTrigger } from '../ui/popover'
+	import PopoverContent from '../ui/popover/popover-content.svelte'
+	import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+	import GamePopup from './GamePopup.svelte'
 
-    type Props = {
-        games: PlayerMoveItem[]
-    }
+	type Props = {
+		games: PlayerMoveItem[]
+	}
 
-    type GameWithTier = PlayerMoveItem & {tier: Tier}
+	type GameWithTier = PlayerMoveItem & { tier: Tier }
 
-    let { games }: Props = $props()
+	let { games }: Props = $props()
 
-    const tiersList = TIERS.map(tier => tier.rank).toReversed()
+	const tiersList = TIERS.map((tier) => tier.rank).toReversed()
 
-    const gamesWithTier: GameWithTier[] = $derived(games.map(game => ({
-        ...game,
-        tier: scoreToTier(game.item_rating)
-    })))
+	const gamesWithTier: GameWithTier[] = $derived(
+		games.map((game) => ({
+			...game,
+			tier: scoreToTier(game.item_rating)
+		}))
+	)
 
-    const gamesPerTier = $derived.by(() => {
-        const result: Record<Tier['rank'], GameWithTier[]> = Object.fromEntries(
-            tiersList.map(tier => [tier, [] as GameWithTier[]])
-        ) as Record<Tier['rank'], GameWithTier[]>
-        
-        for (const game of gamesWithTier) {
-            result[game.tier.rank].push(game)
-        }
+	const gamesPerTier = $derived.by(() => {
+		const result: Record<Tier['rank'], GameWithTier[]> = Object.fromEntries(
+			tiersList.map((tier) => [tier, [] as GameWithTier[]])
+		) as Record<Tier['rank'], GameWithTier[]>
 
-        for (const tier of tiersList) {
-            result[tier].sort((a, b) => b.item_rating - a.item_rating)
-        }
-        
-        return result
-    })
+		for (const game of gamesWithTier) {
+			result[game.tier.rank].push(game)
+		}
+
+		for (const tier of tiersList) {
+			result[tier].sort((a, b) => b.item_rating - a.item_rating)
+		}
+
+		return result
+	})
 </script>
 
 <div>
-   {#each tiersList as tier, idx (tier)}
-        {@const isFirst = idx === 0}
-        {@const tierObj = TIERS.find(t => t.rank === tier)}
-        {@const games = gamesPerTier[tier] ?? []}
-        <div class="flex border-1 border-black {isFirst ? '' : 'border-t-0'}">
-            <div class="h-auto min-h-[80px] w-[101px] border-r-1 border-black flex items-center justify-center text-background" style="background-color: {tierObj?.color}">
-                {tierObj?.label}
-            </div>
-            <div class="bg-[#1A1A17] flex flex-wrap w-[700px] gap-[1px]">
-                {#each games as game (game.id)}
-                    <div class="">
-                    {#if game.cover_image_url}
-                        <img class="h-[80px] w-auto" src={game.cover_image_url} alt={game.item_title} />
-                    {:else}
-                        <div class="h-[80px] w-auto max-w-[80px] bg-gray-800 flex items-center justify-center text-center">
-                            <span class="text-white text-xs break-words">{game.item_title}</span>
-                        </div>
-                    {/if}
-                    </div>
-                {/each}
-            </div>
-        </div>
-   {/each}
+	{#each tiersList as tier, idx (tier)}
+		{@const isFirst = idx === 0}
+		{@const tierObj = TIERS.find((t) => t.rank === tier)}
+		{@const games = gamesPerTier[tier] ?? []}
+		<div class="flex border-1 border-black {isFirst ? '' : 'border-t-0'}">
+			<div
+				class="flex h-auto min-h-[80px] w-[101px] items-center justify-center border-r-1 border-black text-background"
+				style="background-color: {tierObj?.color}"
+			>
+				{tierObj?.label}
+			</div>
+			<div class="flex w-[700px] flex-wrap gap-[1px] bg-[#1A1A17]">
+				{#each games as game (game.id)}
+					<Tooltip>
+						<TooltipTrigger>
+							<div class="">
+								{#if game.cover_image_url}
+									<img class="h-[80px] w-auto" src={game.cover_image_url} alt={game.item_title} />
+								{:else}
+									<div
+										class="flex h-[80px] w-auto max-w-[80px] items-center justify-center bg-gray-800 text-center"
+									>
+										<span class="text-xs break-words text-white">{game.item_title}</span>
+									</div>
+								{/if}
+							</div>
+						</TooltipTrigger>
+						<TooltipContent>
+							<GamePopup {game} />
+						</TooltipContent>
+					</Tooltip>
+				{/each}
+			</div>
+		</div>
+	{/each}
 </div>
