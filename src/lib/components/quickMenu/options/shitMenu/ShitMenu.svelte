0707387d452bf8c24bf422aa8cc7f05261c58ead
Maxim Velesyuk <max.velesyuk@gmail.com>
2026-08-15 13:51:22 +0200

    update layout

--- src/lib/components/quickMenu/options/shitMenu/ShitMenu.svelte
+++ src/lib/components/quickMenu/options/shitMenu/ShitMenu.svelte
@@ -6,32 +6,53 @@
 	import { getAppManager } from '$lib/stores/AppManager.svelte'
 
 	const app = getAppManager()
+
+	// 	<!--
+	// <div class="flex items-center gap-2">
+	//   <!-- Large component + top -->
+	//   <div class="relative">
+	//     <div class="absolute bottom-full left-1/2 -translate-x-1/2">
+	//       Top
+	//     </div>
+
+	//     <div>
+	//       Large
+	//     </div>
+	//   </div>
+
+	//   <!-- Side component -->
+	//   <div>
+	//     Side
+	//   </div>
+	// </div>
+	//  -->
 </script>
 
-<div class="flex gap-20 font-extrabold">
-<div class="flex">
-	<div class="flex flex-col gap-2">
-		<div class="ml-6 flex items-center gap-1">
-			<FireIcon /> Подсёры
-		</div>
-		<div class="flex items-center gap-3">
-			<div class="font-bold w-[160px] bg-secondary rounded-[22px] text-4xl text-center p-2">{app.myPlayer?.shit_stacks}</div>
-			<div class="flex items-center gap-2 flex-1">
-				<AddShitDialog />
+<div class="mt-10 flex gap-20 font-extrabold">
+	<div class="flex items-center gap-2">
+		<div class="relative">
+			<div
+				class="absolute bottom-full left-1/2 mb-2 flex -translate-x-1/2 items-center gap-1 uppercase"
+			>
+				<FireIcon /> Подсёры
+			</div>
+			<div class="w-[160px] rounded-[22px] bg-secondary p-2 text-center text-4xl font-bold">
+				{app.myPlayer?.shit_stacks}
 			</div>
 		</div>
+		<AddShitDialog />
 	</div>
-	
-</div>
-	<div class="flex flex-col gap-2">
-		<div class="ml-6 flex items-center gap-1">
-			<ShieldIcon /> Щиты
-		</div>
-		<div class="flex items-center gap-3">
-			<div class="font-bold w-[160px] bg-secondary rounded-[22px] text-4xl text-center p-2">{app.myPlayer?.shield_stacks}</div>
-			<div class="flex items-center gap-2 flex-1">
-				<MakeShieldDialog />
+	<div class="flex items-center gap-2">
+		<div class="relative">
+			<div
+				class="absolute bottom-full left-1/2 mb-2 flex -translate-x-1/2 items-center gap-1 uppercase"
+			>
+				<ShieldIcon /> Щиты
+			</div>
+			<div class="w-[160px] rounded-[22px] bg-secondary p-2 text-center text-4xl font-bold">
+				{app.myPlayer?.shield_stacks}
 			</div>
 		</div>
+		<MakeShieldDialog />
 	</div>
 </div>
